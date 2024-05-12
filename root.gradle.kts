6336571a175c358726cadff480bd1085a98f8779
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-05-12 02:35:21 -0700

    start multiver shenanigans

---
+++ root.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.9.10" apply false
+    id("org.polyfrost.multi-version.root")
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+}
+
+preprocess {
+    val fabric12001 = createNode("1.20.1-fabric", 12001, "yarn")
+    val fabric12002 = createNode("1.20.2-fabric", 12002, "yarn")
+    val fabric12004 = createNode("1.20.4-fabric", 12004, "yarn")
+    val fabric12006 = createNode("1.20.6-fabric", 12006, "yarn")
+
+
+    fabric12006.link(fabric12004)
+    fabric12004.link(fabric12002)
+    fabric12002.link(fabric12001)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+	repositories {
+		maven("https://maven.fabricmc.net/")
+		maven("https://repo.polyfrost.org/releases")
+		mavenCentral()
+		gradlePluginPortal()
+		plugins {
+			val egtVersion = "0.5.1"
+			id("org.polyfrost.multi-version.root") version egtVersion
+		}
+	}
+}
+
+listOf(
+	"1.20.1-fabric",
+	"1.20.2-fabric",
+	"1.20.4-fabric",
+	"1.20.6-fabric"
+).forEach { version ->
+	include(":$version")
+	project(":$version").apply {
+		projectDir = file("versions/$version")
+		buildFileName = "../../build.gradle"
+	}
+}
+
+rootProject.buildFileName = "root.gradle.kts"
