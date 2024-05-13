451e3a1b57651ddd7513c1886e8ffda2ccaf41e7
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-05-13 00:54:47 -0700

    push change

--- root.gradle.kts
+++ root.gradle.kts
@@ -9,7 +9,6 @@ preprocess {
     val fabric12004 = createNode("1.20.4-fabric", 12004, "yarn")
     val fabric12006 = createNode("1.20.6-fabric", 12006, "yarn")
 
-
     fabric12006.link(fabric12004)
     fabric12004.link(fabric12001)
 }
